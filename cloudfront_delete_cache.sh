12f53d73d6beb2ae2f578b65758c4fa4ecfa2118
skcc00000app08542 <54305632+skcc00000app08542@users.noreply.github.com>
2019-11-07 21:55:39 +0900

    Update cloudfront_delete_cache.sh

--- cloudfront_delete_cache.sh
+++ cloudfront_delete_cache.sh
@@ -3,9 +3,9 @@ aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 aws configure set region $3
 response=`aws sts assume-role --role-arn "arn:aws:iam::583004276805:role/datalake-cloudfront-p-role" --role-session-name cloudfront-delete-cache-session`
-STS_SecretAccessKey=`jq '.Credentials.SecretAccessKey' $response`
-STS_SessionToken=`jq '.Credentials.SessionToken' $response`
-STS_AccessKeyId=`jq '.Credentials.AccessKeyId' $response`
+STS_SecretAccessKey=`echo $response | jq '.Credentials.SecretAccessKey'`
+STS_SessionToken=`echo $response | jq '.Credentials.SessionToken'`
+STS_AccessKeyId=`echo $response | jq '.Credentials.AccessKeyId'`
 echo $STS_SecretAccessKey
 echo $STS_SessionToken
 echo $STS_AccessKeyId
