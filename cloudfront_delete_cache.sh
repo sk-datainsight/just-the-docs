719bd083a686c224979d29a3613b9affc7d2aac9
skcc00000app08542 <54305632+skcc00000app08542@users.noreply.github.com>
2019-11-11 16:39:01 +0900

    Update cloudfront_delete_cache.sh

--- cloudfront_delete_cache.sh
+++ cloudfront_delete_cache.sh
@@ -9,11 +9,14 @@ STS_AccessKeyId=`echo $response | jq '.Credentials.AccessKeyId' | xargs`
 echo $STS_SecretAccessKey
 echo $STS_SessionToken
 echo $STS_AccessKeyId
-aws configure set aws_access_key_id ${STS_AccessKeyId}
-aws configure set aws_secret_access_key ${STS_SecretAccessKey}
-aws configure set region $3
-aws configure set aws_session_token ${STS_SessionToken}
-aws configure list
+echo "export AWS_ACCESS_KEY_ID=${STS_AccessKeyId}" >> $BASH_ENV
+echo "export AWS_SECRET_ACCESS_KEY=${STS_SecretAccessKey}" >> $BASH_ENV
+echo "export AWS_SESSION_TOKEN=${STS_SessionToken}" >> $BASH_ENV
+#aws configure set aws_access_key_id ${STS_AccessKeyId}
+#aws configure set aws_secret_access_key ${STS_SecretAccessKey}
+#aws configure set region $3
+#aws configure set aws_session_token ${STS_SessionToken}
+#aws configure list
 cat ~/.aws/credentials
 aws s3 ls
 #aws cloudfront create-invalidation --distribution-id E378R6TB0VOPAP --paths "/*"
